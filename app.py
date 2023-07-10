c8e85aa6861242276f0fd0e780d35de2a3526c65
Taylor <taylorrosenblum@gmail.com>
2023-07-09 21:05:08 -0400

    -Updated place_text function to determine bounding box of all faces then just place text in upper or lower margin -Added a default text location for cases when no face is detected -Changed face finding model, previous one had too many false positives -Added handling for failing to load image -Added text thickness scaling -Reduced maxium text size -Added feature to change rectangle height based on image orientation

--- app.py
+++ app.py
@@ -19,23 +19,36 @@ def find_faces(img):
 
 def text_placement_vertical(img, faces):
     #parameters
-    min_face_size = im_height / 5
+    min_face_size = im_height / 25
     v_space = [0, im_height]
+
+    face_bbox_top = []
+    face_bbox_bot = []
 
     # for every face, draw a blue rectangle
     for x, y, width, height in faces:
-        if height > min_face_size:
+        if len(faces) >= 10:
+            if height > min_face_size:
+                cv2.rectangle(img, (x, y), (x + width, y + height), color=(255, 0, 0), thickness=2)
+                cv2.line(img, pt1=(0, y + height), pt2=(im_width, y + height), color=(0, 255, 0), thickness=1)
+                cv2.line(img, pt1=(0, y), pt2=(im_width, y), color=(0, 255, 0), thickness=1)
+            face_bbox_top.append(y - height)
+            face_bbox_bot.append(y + height)
+        else:
             cv2.rectangle(img, (x, y), (x + width, y + height), color=(255, 0, 0), thickness=2)
-            cv2.line(img, pt1=(0, y + height), pt2=(im_width, y + height), color=(0, 255, 0), thickness=5)
-            cv2.line(img, pt1=(0, y), pt2=(im_width, y), color=(0, 255, 0), thickness=5)
-            v_space.extend([y, y + height])
+            cv2.line(img, pt1=(0, y + height), pt2=(im_width, y + height), color=(0, 255, 0), thickness=1)
+            cv2.line(img, pt1=(0, y), pt2=(im_width, y), color=(0, 255, 0), thickness=1)
+            face_bbox_top.append(y - height)
+            face_bbox_bot.append(y + height)
 
-    v_space = sorted(v_space)
-    v_space_diff = numpy.diff(sorted(v_space)) # measure the spacing between adjacent horizontals
-    print("v_space_diff: {}".format(v_space_diff))
-    max_diff = numpy.argmax(v_space_diff) # indentify the index of the largest gap
-    print("max vertical space between line {} and line {}".format(max_diff, max_diff + 1))
-    center_max_space = int((v_space[max_diff] + v_space[max_diff + 1]) / 2)
+
+    margin_top = min(face_bbox_top)
+    margin_bot = im_height - max(face_bbox_bot)
+
+    if margin_top > margin_bot:
+        center_max_space = int(margin_bot / 2)
+    else:
+        center_max_space = int((im_height + max(face_bbox_bot)) / 2)
 
     # draw
     cv2.line(img, pt1=(0, center_max_space), pt2=(im_width, center_max_space), color=(0, 0, 255), thickness=5)
@@ -46,9 +59,14 @@ def text_placement_vertical(img, faces):
 
 def place_text(text, img, center_max_space):
     # rectangle params
-    rect_width = int(0.75 * im_width)  # 50% the width
-    rect_height = int(0.1 * im_height)  # 10% the height of the image
     im_center = int(im_width * 0.5)
+    rect_width = int(0.75 * im_width)  # 50% the width
+
+    if im_width > im_height:
+        rect_height = int(0.1 * im_height)  # 10% the height of landscape images
+    else:
+        rect_height = int(0.05 * im_height)  # 5% the height of vertical images
+
 
     start_point = (im_center - int(rect_width * 0.5),
                    center_max_space - int(rect_height * 0.5))  # represents the top left corner of rectangle
@@ -68,10 +86,10 @@ def place_text(text, img, center_max_space):
     org = (im_center - int(rect_width * 0.5) + text_margin_x,
            center_max_space + int(rect_height * 0.5) - text_margin_y)  # text origin
     color = (255, 255, 255)  # Blue color in BGR
-    text_thickness = 3
+    text_thickness = int(im_width / 300)
 
     def get_optimal_font_scale(text, width):
-        for scale in reversed(range(0, 60, 1)):
+        for scale in reversed(range(0, 30, 1)):
             textSize = cv2.getTextSize(text, font, fontScale=scale / 10, thickness=1)
             new_width = textSize[0][0]
             if (new_width <= width):
@@ -85,14 +103,26 @@ def place_text(text, img, center_max_space):
 
     cv2.imwrite("output/img_text.jpg", img)
 
-# initialize the face recognizer (default face haar cascade)
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-print(face_cascade.empty())
 
 #bring in the photo
 img = cv2.imread(args.fname)
-im_height, im_width, channels = img.shape
 
-faces = find_faces(img)
-center_max_space = text_placement_vertical(img,faces)
-place_text(args.text, img, center_max_space)
+if img is None:
+    print('image did not load')
+
+else:
+    # initialize the face recognizer (default face haar cascade)
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_alt.xml')
+    print(face_cascade.empty())
+
+    im_height, im_width, channels = img.shape
+
+    faces = find_faces(img)
+
+
+    if len(faces)==0:
+        center_max_space = int(im_height * 0.75)
+    else:
+        center_max_space = text_placement_vertical(img,faces)
+
+    place_text(args.text, img, center_max_space)
